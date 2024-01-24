3ae05776c634c2e281ac6db6e195fd77601e6d04
Nikunj2303 <142402711+Nikunj2303@users.noreply.github.com>
2024-01-24 17:52:38 +0530

    Delete alias directory

--- alias/models/product_template.py
+++
@@ -1,7 +0,0 @@
-
-from odoo import models, fields, api
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    alias_ids = fields.One2many('product.template','alias_ids', string='Aliases')
